9afbd56ba17942ef2d35093ed08c7c9e2831a315
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-05-11 18:35:36 +0100

    Added polls app to config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', # This is the app we created
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

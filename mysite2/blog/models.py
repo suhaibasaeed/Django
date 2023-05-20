b0e5747f0d99b102d9037c2c693504852113a0d6
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-05-20 21:58:07 +0100

    Added default sort order

--- mysite2/blog/models.py
+++ mysite2/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # Data model for blog posts
@@ -11,8 +12,20 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     # Textfield corresponds to TEXT column in SQL DB - 
     body = models.TextField()
+    # Date and time of publication - DATETIME column in SQL DB
+    publish = models.DateTimeField(default=timezone.now)
+    # Store when object was created
+    created = models.DateTimeField(auto_now_add=True)
+    # Store when object was last updated - auto update date when object is saved
+    updated = models.DateTimeField(auto_now=True)
 
+    # Meta class to specify model-specific options
+    class Meta:
+        # Order results by publish field in descending order
+        ordering = ('-publish',)
+    
     def __str__(self):
         """ Return string representation of object"""
         return self.title
+    
     

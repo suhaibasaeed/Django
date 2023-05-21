32cb97f89affc090351aaf885e2bc5f8e140cf59
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-05-21 03:21:56 +0100

    Added extras to admin page

--- mysite2/blog/admin.py
+++ mysite2/blog/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    # Auto populate slug field with title field
+    prepopulated_fields = {'slug': ('title',)}
+    # Filter by author field
+    raw_id_fields = ('author',)
+    # Navigation links to navigate through date hierarchy
+    date_hierarchy = 'publish'
